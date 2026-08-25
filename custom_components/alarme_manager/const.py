3960b0b1afcf417270b365fbe70e51c90478b20b
cybersquatbis <hanna.alain56@gmail.com>
2026-08-25 11:21:18 +0200

    Community 0.4: add trace and temperature constants

--- custom_components/alarme_manager/const.py
+++ custom_components/alarme_manager/const.py
@@ -1,36 +1,13 @@
 """Constants for Alarme Manager Community."""
-
-from __future__ import annotations
-
 from typing import Final
-
 DOMAIN: Final = "alarme_manager"
 NAME: Final = "Alarme Manager Community"
-VERSION: Final = "0.3.0-beta.1"
-
+VERSION: Final = "0.4.0-beta.1"
 PANEL_URL: Final = "alarme-manager"
 STATIC_URL: Final = "/alarme_manager_static"
 STORAGE_KEY: Final = "alarme_manager.storage"
-STORAGE_VERSION: Final = 1
+STORAGE_VERSION: Final = 2
 SIGNAL_UPDATE: Final = f"{DOMAIN}_update"
-
-CONF_NAME: Final = "name"
-CONF_ALARM_ENTITY: Final = "alarm_entity"
-CONF_OPENINGS: Final = "openings"
-CONF_MOTIONS: Final = "motions"
-CONF_SMOKE_ENTITIES: Final = "smoke_entities"
-CONF_CAMERAS: Final = "cameras"
-CONF_LIGHTS: Final = "lights"
-CONF_AUX_DEVICES: Final = "aux_devices"
-CONF_SIREN: Final = "siren"
-CONF_CRITICAL_ENTITIES: Final = "critical_entities"
-CONF_RF_ENTITIES: Final = "rf_entities"
-CONF_RAIN_ENTITY: Final = "rain_entity"
-CONF_SIREN_DURATION: Final = "siren_duration"
-
-DEFAULT_NAME: Final = "Maison"
-DEFAULT_SIREN_DURATION: Final = 30
-MAX_INCIDENTS: Final = 100
-MAX_TEST_DURATION: Final = 10
-
-PLATFORMS: Final = ["sensor", "binary_sensor"]
+CONF_NAME="name"; CONF_ALARM_ENTITY="alarm_entity"; CONF_OPENINGS="openings"; CONF_MOTIONS="motions"; CONF_SMOKE_ENTITIES="smoke_entities"; CONF_CAMERAS="cameras"; CONF_LIGHTS="lights"; CONF_AUX_DEVICES="aux_devices"; CONF_SIREN="siren"; CONF_CRITICAL_ENTITIES="critical_entities"; CONF_RF_ENTITIES="rf_entities"; CONF_RAIN_ENTITY="rain_entity"; CONF_SIREN_DURATION="siren_duration"; CONF_TEMPERATURE_ENTITIES="temperature_entities"
+DEFAULT_NAME="Maison"; DEFAULT_SIREN_DURATION=30; MAX_INCIDENTS=100; MAX_TEST_DURATION=10; MAX_TRACE_POINTS=100
+PLATFORMS=["sensor"]
