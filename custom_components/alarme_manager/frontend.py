025d45c1386941b2eb41bbfaf6184016c113d9ce
cybersquatbis <hanna.alain56@gmail.com>
2026-08-26 08:43:38 +0200

    Community 0.5: load frontend compatibility patch

--- custom_components/alarme_manager/frontend.py
+++ custom_components/alarme_manager/frontend.py
@@ -9,6 +9,6 @@ ASSET_DIR=Path(__file__).parent
 async def async_register_frontend(hass):
     await hass.http.async_register_static_paths([StaticPathConfig(STATIC_URL,str(ASSET_DIR),cache_headers=False)])
     if PANEL_URL not in hass.data.get("frontend_panels",{}):
-        await panel_custom.async_register_panel(hass=hass,frontend_url_path=PANEL_URL,webcomponent_name="alarme-manager-panel",module_url=f"{STATIC_URL}/alarme-manager.js",sidebar_title="Alarme Manager",sidebar_icon="mdi:shield-home",require_admin=False)
+        await panel_custom.async_register_panel(hass=hass,frontend_url_path=PANEL_URL,webcomponent_name="alarme-manager-panel",module_url=f"{STATIC_URL}/community-0.5.js",sidebar_title="Alarme Manager",sidebar_icon="mdi:shield-home",require_admin=False)
 def async_unregister_frontend(hass):
     if PANEL_URL in hass.data.get("frontend_panels",{}):async_remove_panel(hass,PANEL_URL)
